04fc31926750745f28be777294f986da6bfcd567
TomLu1323241 <Yuming.tom.lu@gmail.com>
2022-05-01 23:45:52 -0400

    trying to fix endpoint for comments

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // Trying original sanity client fix at 2:26:55
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { sanityClient } from '../../sanity';
+import { createClient } from 'next-sanity';
 import { BlogComment } from '../../typings';
 
 type Data = {
@@ -13,6 +13,15 @@ export default function createComment(
   res: NextApiResponse<Data>
 ) {
   const body: BlogComment = JSON.parse(req.body);
+  const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    token: process.env.SANITY_API_TOKEN,
+    apiVersion: '2021-03-25',
+    useCdn: process.env.USE_CDN == '1',
+  };
+
+  const sanityClient = createClient(config);
   sanityClient.create({
     _type: 'comment',
     post: {
--- sanity.js
+++ sanity.js
@@ -1,14 +1,14 @@
 import {
   createCurrentUserHook,
   createClient,
-} from "next-sanity";
+} from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   token: process.env.SANITY_API_TOKEN,
-  apiVersion: "2021-03-25",
+  apiVersion: '2021-03-25',
   useCdn: process.env.USE_CDN == '1',
 };
 
