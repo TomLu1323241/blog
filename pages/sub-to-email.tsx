f964a4efd3157a02b9e92a7920d579ec2bd6f51e
TomLu1323241 <Yuming.tom.lu@gmail.com>
2022-06-06 18:31:59 -0400

    tells the user when they will get emails

--- pages/sub-to-email.tsx
+++ pages/sub-to-email.tsx
@@ -69,7 +69,7 @@ export default function EmailVerification() {
               Please enter your email:
             </h1>
             <p className='text-gray-500 text-sm pt-2 text-center'>Note: This will probably end up in your spam email and you need to <span className='text-red-500'>tell your email account this is not spam</span></p>
-            <p className='text-gray-500 text-sm pb-4 pt-2 text-center'>For the moment, emails will not be sent to anyone besides the verification email. I've not built the email subscription service yet 😐😐😐</p>
+            <p className='text-gray-500 text-sm pb-4 pt-2 text-center'>Emails are sent out every Saturday at 8:00am informing you of that week's blog posts!</p>
           </>
         }
         {submittedEmail == SubmittedProgress.NotSubmitted &&
