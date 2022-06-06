e0edc5bd084b04f49f1fb908b5b9ef53280c8af9
TomLu1323241 <Yuming.tom.lu@gmail.com>
2022-06-06 09:58:06 -0400

    frontend working but no error handling

--- pages/sub-to-email.tsx
+++ pages/sub-to-email.tsx
@@ -5,6 +5,7 @@ import { SubmittedProgress } from '../enums';
 import { Email, EmailCode } from '../typings';
 import { LoadingGifs } from '../consts';
 import VerificationInput from 'react-verification-input';
+import Link from 'next/link';
 
 export default function EmailVerification() {
   const [email, setEmail] = useState<string>('');
@@ -30,6 +31,7 @@ export default function EmailVerification() {
   };
 
   const [submittedCode, setSubmittedCode] = useState<SubmittedProgress>(SubmittedProgress.NotSubmitted);
+  const [codeError, setCodeError] = useState<boolean>(false);
   const {
     register: registerCode,
     handleSubmit: handleSubmitCode,
@@ -37,14 +39,17 @@ export default function EmailVerification() {
     setValue: setValueCode,
   } = useForm<EmailCode>();
   const onSummitCode: SubmitHandler<EmailCode> = async (data: EmailCode) => {
-    // console.log(data);
-    // return;
+    if (codeError || data.code.length != 6) {
+      setCodeError(true);
+      return;
+    }
+    setSubmittedCode(SubmittedProgress.Submitting);
     const res = await fetch('/api/verifyEmailCode', {
       method: 'POST',
       body: JSON.stringify(data),
     });
     if (res.ok) {
-      console.log('submitted comment');
+      setSubmittedCode(SubmittedProgress.Submitted);
     } else {
       console.log(`${res.status} : ${res.statusText}`);
     }
@@ -85,40 +90,67 @@ export default function EmailVerification() {
           <img className='h-40 mx-auto' src={LoadingGifs[Math.floor(Math.random() * LoadingGifs.length)]} />
         }
 
+        {(submittedEmail == SubmittedProgress.Submitted && submittedCode != SubmittedProgress.Submitted) &&
+          <>
+            <h1 className='text-3xl mx-auto pt-4 text-center'>
+              Please check your email: <span className='text-gray-500'>{email}</span>
+            </h1>
+            <p className='text-gray-500 text-sm pb-4 pt-2 text-center'>Note: This will probably end up in your spam email and you need to tell your email account this is not spam</p>
+          </>
+        }
+
         {(submittedEmail == SubmittedProgress.Submitted && submittedCode == SubmittedProgress.NotSubmitted) &&
-          <form onSubmit={handleSubmitCode(onSummitCode)}>
+          <form onSubmit={handleSubmitCode(onSummitCode)} className='flex flex-col justify-center'>
             <input
               {...registerCode('email')}
-              // Set require to true
               type='hidden'
               value={email}
             />
-            <div className='flex flex-row align-middle'>
+            {/* TODO: make this look a bit prettier */}
+            <div className='flex flex-row align-middle mx-auto justify-center'>
               <VerificationInput
                 removeDefaultStyles
                 validChars='[a-z|0-9]*'
                 placeholder='·'
                 onChange={(e) => {
                   setValueCode('code', e);
+                  if (e.length === 6) {
+                    setCodeError(false);
+                  }
                 }}
                 classNames={{
-                  container: 'h-24 flex flex-row',
+                  container: 'h-24 flex flex-row mx-auto',
                   character: 'w-16 text-6xl leading-[5.5rem]'
                 }}
               />
             </div>
             <button
               type='submit'
-              className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold mt-4 mb-2 py-2 px-4 rounded cursor-pointer w-full'>
+              className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold mt-4 mb-2 py-2 px-4 rounded cursor-pointer max-w-3xl mx-auto'>
               Verify Email!
             </button>
-            {errorsCode.email && (
-              <span className='text-red-500'>- The Email field is required</span>
-            )}
-            {errorsCode.code && (
-              <span className='text-red-500'>- The code field is required</span>
+            {codeError && (
+              <span className='text-red-500'>- The code is a 6 digit code, please try again</span>
             )}
           </form>
+        }
+
+        {submittedCode == SubmittedProgress.Submitting &&
+          <img className='h-40 mx-auto' src={LoadingGifs[Math.floor(Math.random() * LoadingGifs.length)]} />
+        }
+
+        {submittedCode == SubmittedProgress.Submitted &&
+          <>
+            <h1 className='text-3xl mx-auto pt-4'>
+              Thanks for subscribing to Tom's Blog!!!
+            </h1>
+            <Link href={'/'} passHref>
+              <a
+                className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold mt-4 mb-2 py-2 px-4 rounded cursor-pointer w-full text-center'>
+                Return to home page
+              </a>
+            </Link>
+          </>
         }
       </div>
     </div>
