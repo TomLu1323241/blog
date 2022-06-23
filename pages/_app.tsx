9f595c6db217a622ed31fe847d0cf0e01e3260ea
TomLu1323241 <Yuming.tom.lu@gmail.com>
2022-06-23 16:10:50 -0400

    working toggle between light and dark mode, need to add more css to other parts to make it look okay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider enableColorScheme={true} attribute='class'><Component {...pageProps} /></ThemeProvider>;
 }
 
 export default MyApp;
