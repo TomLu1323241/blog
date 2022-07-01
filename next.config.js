979bb14881bcdd70576df7efde8dfed393c56285
TomLu1323241 <Yuming.tom.lu@gmail.com>
2022-07-01 13:42:11 -0400

    fs webpack fix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io']
-  }
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 };
