442baa6c8317a2685f9c1b4e063bc000953879de
Tom Lu <tlu@essentialaccessibility.com>
2022-07-14 15:44:44 -0400

    try revalidate media index page and media slug page

--- pages/media/[slug].tsx
+++ pages/media/[slug].tsx
@@ -58,6 +58,7 @@ export default function Archives({ title, archives, slug, size }: Props) {
       setImages(images => [...newArchive, ...images]);
       setFetchSize(fetchSize + 1);
       reset({ link: '' });
+      await fetch(`/api/revalidate?path=/media`);
       await fetch(`/api/revalidate?path=/media/${slug}`);
     } else {
       setSubmittingImage(SubmittedProgress.NotSubmitted);
